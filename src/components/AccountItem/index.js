dbb43c75d9e824a18fbf09bd49b971382e026aa6
ngocuong cuong <ngocuongit2508@gmail.com>
2022-05-19 00:22:29 +0700

    Custom search-header #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/e99846f00ab6702d7716ba8d72a98d7a~c5_100x100.jpeg?x-expires=1653055200&x-signature=%2F%2BDDHwm%2B6BcrRIDdztHGkRZAkXI%3D"
+                alt="hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('icon')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('userName')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
